99830720cd2e36f8c005220e0d58af8876ee84da
Andrew Bentzen <bentzen.an@gmail.com>
2022-03-03 11:02:01 -0500

    adds a short writeup on the homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -16,6 +16,39 @@ const Home = () => {
           https://github.com/bentzen-andy/cis658-hw15-client-side-scripting
         </a>
       </p>
+
+      <h2>Mini-Project 1: DataGrid</h2>
+      <p>
+        For this I just hardcoded some json data to create the table. I used React's state
+        management to control whether each row should be shown. The "remove" button simply turns it
+        off.{" "}
+      </p>
+
+      <h2>Mini-Project 2: Form Validation</h2>
+      <p>
+        On the form I used a custom React hook to control the state of the form validation messages.
+        Using a custom hook in this way makes the React code a little more readable. The basic idea
+        behind this validation is to dynamically check if required fields have been filled in
+        properly. There's also check to make sure the user actually had a chance to click into the
+        field before it yells at you with nasty validation text. Additionally, the submit button is
+        unclickable until each field is properly filled in. The validation scheme is borrowed
+        heavily (but not copied) from this online course in React:{" "}
+        <a href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/">
+          https://www.udemy.com/course/react-the-complete-guide-incl-redux/
+        </a>
+        . And I also used a regex that I borrowed from this Stack Overflow forum (it did not make
+        sense me to rewrite an email regex from scratch):
+        <a href="https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript">
+          https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+        </a>
+      </p>
+
+      <h2>Mini-Project 3: Annoying Button</h2>
+      <p>
+        For this one, I simply created an event-handler on the button and adjusted the position via
+        the styles. Nothing too complex here. The button sits in a div, and I just set the button to
+        have a "relative" position.
+      </p>
     </React.Fragment>
   );
 };
